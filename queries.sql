704a103570591da2ed7a404180507411515e1d31
Андрей <39153320+samsam4055@users.noreply.github.com>
2019-04-27 21:37:39 +0300

    БД
    тестирование на дату  окончания лота

--- queries.sql
+++ queries.sql
@@ -7,11 +7,11 @@ INSERT INTO users
 
 INSERT INTO lots
 (id, name, description, img_url, start_price, end_at, rate_step, user_id, category_id) VALUES
-	('1', '2014 Rossignol District Snowboard', 'Описание лота 1', 'img/lot-1.jpg', '10999', '2019-04-20', '100', '1', '1'),
-	('2', 'DC Ply Mens 2016/2017 Snowboard', 'Описание лота 2', 'img/lot-2.jpg', '159999', '2019-04-21', '100', '2', '1'),
+	('1', '2014 Rossignol District Snowboard', 'Описание лота 1', 'img/lot-1.jpg', '10999', '2019-04-29', '100', '1', '1'),
+	('2', 'DC Ply Mens 2016/2017 Snowboard', 'Описание лота 2', 'img/lot-2.jpg', '159999', '2019-04-28', '100', '2', '1'),
 	('3', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Описание лота 3', 'img/lot-3.jpg', '8000', '2019-04-22', '100', '2', '2'),
-	('4', 'Ботинки для сноуборда DC Mutiny Charocal', 'Описание лота 4', 'img/lot-4.jpg', '10999', '2019-04-23', '100', '2', '3'),
-	('5','Куртка для сноуборда DC Mutiny Charocal', 'Описание лота 5', 'img/lot-5.jpg', '7500', '2019-04-21', '100', '1', '4'),
+	('4', 'Ботинки для сноуборда DC Mutiny Charocal', 'Описание лота 4', 'img/lot-4.jpg', '10999', '2019-04-30', '100', '2', '3'),
+	('5','Куртка для сноуборда DC Mutiny Charocal', 'Описание лота 5', 'img/lot-5.jpg', '7500', '2019-04-27', '100', '1', '4'),
 	('6','Маска Oakley Canopy', 'Описание лота 6', 'img/lot-6.jpg', '5400', '2019-04-22', '100', '1', '6');
 
 INSERT INTO rates
@@ -24,7 +24,7 @@ SELECT * FROM categories;
 SELECT l.name, l.img_url, l.start_price, c.name, r.amount FROM lots AS l
     LEFT JOIN categories AS c ON l.category_id = c.id
 		LEFT JOIN rates AS r ON l.category_id = r.id
-    WHERE l.winner_id IS NULL
+    WHERE l.winner_id IS NULL AND l.end_at >= CURDATE()
 		ORDER BY l.created_at DESC
 		LIMIT 5;
 
