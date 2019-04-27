3518406a6b85d849bcc12dc1d816207c9e4f0afe
Андрей <39153320+samsam4055@users.noreply.github.com>
2019-04-27 19:50:45 +0300

    SQL-запросы
    подправил названия

--- queries.sql
+++ queries.sql
@@ -6,7 +6,7 @@ INSERT INTO users
 (id, email, name, password, contact, avatar_url) VALUES ('1', 'a1@mail.ua', 'Вася', 'pass1', 'улица Пушкина', 'img/ava1.jpg'), ('2', 'a2@mail.ua', 'Петя', 'pass2', 'улица Шевченко', 'img/ava2.jpg');
 
 INSERT INTO lots
-(id, name, description, img, start_price, end_at, rate_step, user_id, category_id) VALUES
+(id, name, description, img_url, start_price, end_at, rate_step, user_id, category_id) VALUES
 	('1', '2014 Rossignol District Snowboard', 'Описание лота 1', 'img/lot-1.jpg', '10999', '2019-04-20', '100', '1', '1'),
 	('2', 'DC Ply Mens 2016/2017 Snowboard', 'Описание лота 2', 'img/lot-2.jpg', '159999', '2019-04-21', '100', '2', '1'),
 	('3', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Описание лота 3', 'img/lot-3.jpg', '8000', '2019-04-22', '100', '2', '2'),
@@ -21,7 +21,7 @@ INSERT INTO rates
 SELECT * FROM categories;
 
 /* получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории */
-SELECT l.name, l.img, l.start_price, c.name, r.amount FROM lots AS l
+SELECT l.name, l.img_url, l.start_price, c.name, r.amount FROM lots AS l
     LEFT JOIN categories AS c ON l.category_id = c.id
 		LEFT JOIN rates AS r ON l.category_id = r.id
     WHERE l.winner_id IS NULL
