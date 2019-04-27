aa80bc8a03f7e6ce7fe5a08d21ea8a3a58d1f941
Андрей <39153320+samsam4055@users.noreply.github.com>
2019-04-27 19:32:04 +0300

    запросы БД
    ошибка в id лотов
    редактирование запроса "самых новых, открытых лотов"

--- queries.sql
+++ queries.sql
@@ -6,10 +6,10 @@ INSERT INTO users
 (id, email, name, password, contact, avatar_url) VALUES ('1', 'a1@mail.ua', 'Вася', 'pass1', 'улица Пушкина', 'img/ava1.jpg'), ('2', 'a2@mail.ua', 'Петя', 'pass2', 'улица Шевченко', 'img/ava2.jpg');
 
 INSERT INTO lots
-(id, name, description, img, start_price, end_at, rate_step, user_id, category_id) VALUES 
+(id, name, description, img, start_price, end_at, rate_step, user_id, category_id) VALUES
 	('1', '2014 Rossignol District Snowboard', 'Описание лота 1', 'img/lot-1.jpg', '10999', '2019-04-20', '100', '1', '1'),
 	('2', 'DC Ply Mens 2016/2017 Snowboard', 'Описание лота 2', 'img/lot-2.jpg', '159999', '2019-04-21', '100', '2', '1'),
-	('4', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Описание лота 3', 'img/lot-3.jpg', '8000', '2019-04-22', '100', '2', '2'),
+	('3', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Описание лота 3', 'img/lot-3.jpg', '8000', '2019-04-22', '100', '2', '2'),
 	('4', 'Ботинки для сноуборда DC Mutiny Charocal', 'Описание лота 4', 'img/lot-4.jpg', '10999', '2019-04-23', '100', '2', '3'),
 	('5','Куртка для сноуборда DC Mutiny Charocal', 'Описание лота 5', 'img/lot-5.jpg', '7500', '2019-04-21', '100', '1', '4'),
 	('6','Маска Oakley Canopy', 'Описание лота 6', 'img/lot-6.jpg', '5400', '2019-04-22', '100', '1', '6');
@@ -20,10 +20,13 @@ INSERT INTO rates
 /* получить все категории */
 SELECT * FROM categories;
 
-/* получить самые новые, открытые лоты */
-SELECT l.name, l.img, l.start_price, c.name FROM lots AS l
+/* получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории */
+SELECT l.name, l.img, l.start_price, c.name, r.amount FROM lots AS l
     LEFT JOIN categories AS c ON l.category_id = c.id
-    WHERE l.winner_id IS NULL;
+		LEFT JOIN rates AS r ON l.category_id = r.id
+    WHERE l.winner_id IS NULL
+		ORDER BY l.created_at DESC
+		LIMIT 5;
 
 /* показать лот по его id и название категории*/
 SELECT l.id, l.name, c.name FROM lots AS l
