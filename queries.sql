59e63c8ab7365ba6856350e2757f525b86bad2b8
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-04-27 13:46:19 +0300

    Запросы в БД
    Добавил запросы

--- queries.sql
+++ queries.sql
@@ -12,4 +12,28 @@ INSERT INTO lots
 	('Крепления Union Contact Pro 2015 года размер L/XL', 'Описание лота 3', 'img/lot-3.jpg', '8000', '2019-04-22', '100', '2', '2'),
 	('Ботинки для сноуборда DC Mutiny Charocal', 'Описание лота 4', 'img/lot-4.jpg', '10999', '2019-04-23', '100', '2', '3'),
 	('Куртка для сноуборда DC Mutiny Charocal', 'Описание лота 5', 'img/lot-5.jpg', '7500', '2019-04-21', '100', '1', '4'),
-	('Маска Oakley Canopy', 'Описание лота 6', 'img/lot-6.jpg', '5400', '2019-04-22', '100', '1', '6')
+	('Маска Oakley Canopy', 'Описание лота 6', 'img/lot-6.jpg', '5400', '2019-04-22', '100', '1', '6');
+
+INSERT INTO rates
+(amount, user_id, lot_id) VALUES ('12000', '1', '1'), ('170000', '2', '2')
+
+/* получить все категории */
+SELECT * FROM categories;
+
+/* получить самые новые, открытые лоты */
+SELECT l.name, l.img, l.start_price, c.name FROM lots AS l
+    LEFT JOIN categories AS c ON l.category_id = c.id
+    WHERE l.winner_id IS NULL;
+
+/* показать лот по его id и название категории*/
+SELECT l.id, l.name, c.name FROM lots AS l
+    LEFT JOIN categories AS c ON l.category_id = c.id
+    WHERE l.id = 1;
+
+/* обновить название лота по его идентификатору */
+UPDATE lots SET  name = 'Другое имя лота'
+    WHERE id = 1;
+
+/* получить список самых свежих ставок для лота по его идентификатору */
+SELECT id, amount FROM rates
+    WHERE lot_id = 2;
