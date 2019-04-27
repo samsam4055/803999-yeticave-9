c7d3459d0900bf2093ea41ec9fa6cc4dbdbcb727
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-04-27 11:34:41 +0300

    Пишем SQL-запросы
    Создал файл queries.sql
    начал добавлять информацию в БД

---
+++ queries.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO categories
+(name, code) VALUES ('Доски и лыжи', 'skiing'), ('Крепления', 'mounts'), ('Ботинки', 'boots'), ('Одежда', 'clothing'), ('Инструменты', 'tools'), ('Разное', 'other');
+
+INSERT INTO users
+(email, name, password, contact, avatar) VALUES ('a1@mail.ua', 'Вася', 'pass1', 'улица Пушкина', 'img/ava1.jpg'), ('a2@mail.ua', 'Петя', 'pass2', 'улица Шевченко', 'img/ava2.jpg')
