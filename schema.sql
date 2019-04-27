021dd89bbc62c33436eb1d145d0605bb235e93ad
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-04-27 09:26:07 +0300

    Схема БД
    Что-то туго пошло... возможно из-за отсутствия времени... пересмтрю лекции еще раз (какраз выходные) и с новыми силами попробую наверстать...

--- schema.sql
+++ schema.sql
@@ -6,42 +6,40 @@ USE yeticave;
 CREATE TABLE categories (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name CHAR(128) UNIQUE,
-	icon CHAR(64)
+	code CHAR(64) UNIQUE
 );
 
 INSERT INTO categories
-(name,icon) VALUES ('Доски и лыжи', 'skiing'), ('Крепления', 'mounts'), ('Ботинки', 'boots'), ('Одежда', 'clothing'), ('Инструменты', 'tools'), ('Разное', 'other');
+(name, code) VALUES ('Доски и лыжи', 'skiing'), ('Крепления', 'mounts'), ('Ботинки', 'boots'), ('Одежда', 'clothing'), ('Инструменты', 'tools'), ('Разное', 'other');
 
 CREATE TABLE lots (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	dt_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	name CHAR(128) UNIQUE,
-	description MEDIUMTEXT,
-	img CHAR(128),
-	price FLOAT,
-	dt_end DATETIME,
-	rate_step INT,
-	user_id INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	name CHAR(128) NOT NULL,
+	description MEDIUMTEXT NOT NULL,
+	img CHAR(128) NOT NULL,
+	start_price FLOAT NOT NULL,
+	end_at DATETIME NOT NULL,
+	rate_step INT NOT NULL,
+	user_id INT NOT NULL,
 	winner_id INT,
-	category_id INT
+	category_id INT NOT NULL
 );
 
 CREATE TABLE rates (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	amount FLOAT,
-	user_id INT,
-	lot_id INT
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	amount FLOAT NOT NULL,
+	user_id INT NOT NULL,
+	lot_id INT NOT NULL
 );
 
 CREATE TABLE users (
    id INT AUTO_INCREMENT PRIMARY KEY,
-   dt_rg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   email CHAR(128) UNIQUE,
-   name CHAR UNIQUE,
-   password CHAR(64),
-   avatar CHAR(128),
-   contact CHAR(128),
-   lot_id CHAR(128),
-   rate_id CHAR(128)
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   email CHAR(128) UNIQUE NOT NULL,
+   name CHAR NOT NULL,
+   password CHAR(64) NOT NULL,
+   contact CHAR(128) NOT NULL,
+   avatar CHAR(128)
 );
