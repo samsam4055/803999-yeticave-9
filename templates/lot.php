46d7ecd86fca05bf87d8dd8e8b6ea12f5239cc57
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-05-03 11:07:39 +0300

    В шаблоне lot.php берем категории из базы

--- templates/lot.php
+++ templates/lot.php
@@ -1,24 +1,11 @@
 <main>
     <nav class="nav">
       <ul class="nav__list container">
-        <li class="nav__item">
-          <a href="all-lots.html">Доски и лыжи</a>
-        </li>
-        <li class="nav__item">
-          <a href="all-lots.html">Крепления</a>
-        </li>
-        <li class="nav__item">
-          <a href="all-lots.html">Ботинки</a>
-        </li>
-        <li class="nav__item">
-          <a href="all-lots.html">Одежда</a>
-        </li>
-        <li class="nav__item">
-          <a href="all-lots.html">Инструменты</a>
-        </li>
-        <li class="nav__item">
-          <a href="all-lots.html">Разное</a>
-        </li>
+        <?php foreach ($categories as $category) : ?>
+            <li class="nav__item">
+                <a href="all-lots.html"><?= esc($category['name']); ?></a>
+            </li>
+        <?php endforeach; ?>
       </ul>
     </nav>
     <section class="lot-item container">
