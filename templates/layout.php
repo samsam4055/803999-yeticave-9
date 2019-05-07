d85f46e596c613f49eb68872b61afceb4812a54a
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-05-07 17:11:20 +0300

    Подключил стили flatpickr в общий layout

--- templates/layout.php
+++ templates/layout.php
@@ -5,6 +5,7 @@
     <title><?= esc($title);?></title>
     <link href="css/normalize.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+	<link href="css/flatpickr.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="page-wrapper">
