518d8d4083b4d1c7fd28163c4849361fcfae5775
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2019-05-08 12:56:01 +0300

    Валидация формы (начало)

--- add.php
+++ add.php
@@ -6,9 +6,41 @@ require_once 'inc/data.php';
 $categories = get_categories($link);
 
 $title = "Добавление лота";
-$page_content = include_template('add-lot.php', [
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $new_lot = $_POST;
+
+	$required = ['category', 'lot-name', 'message', 'lot-rate', 'lot-step', 'lot-date'];
+
+	$dict = ['category' => 'категория', 'lot-name' => 'имя лота', 'message' => 'описание лота',
+	'lot-rate' => 'cтавка лота', 'lot-step' => 'шаг ставки', 'lot-date' => 'дата лота'
+	];
+
+	$errors = [];
+
+	foreach ($required as $key) {
+		if (empty($_POST[$key])) {
+            $errors[$key] = 'Это поле надо заполнить';
+		}
+	}
+	if (count($errors)) {
+
+	$page_content = include_template('add-lot.php', [
     'categories' => $categories,
+	'errors' => $errors
     ]);
+    }
+	else {
+		header('Location: lot.php?id=5'); //  . $lot_id  переадресация на страницу просмотра лота
+		die();
+	}
+}
+
+else {
+$page_content = include_template('add-lot.php', [
+    'categories' => $categories
+    ]);
+}
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
--- templates/add-lot.php
+++ templates/add-lot.php
@@ -11,15 +11,22 @@
     <form class="form form--add-lot container form--invalid" action="add.php" method="post"> <!-- form--invalid -->
       <h2>Добавление лота</h2>
       <div class="form__container-two">
-        <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+	          <?php $classname = isset($errors['lot-name']) ? "form__item--invalid" : "";
+       	$error_message = isset($errors['lot-name']) ? $errors['lot-name'] : "";
+        $value = isset($user_lot['lot-name']) ? $user_lot['lot-name'] : "";  ?>
+        <div class="form__item <?=isset($errors['lot-name']) ? "form__item--invalid" : "";?>">
+        
           <label for="lot-name">Наименование <sup>*</sup></label>
           <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота">
           <span class="form__error">Введите наименование лота</span>
         </div>
-        <div class="form__item">
+        <?php $classname = isset($errors['category']) ? "form__item--invalid" : "";
+        $error_message = isset($errors['category']) ? $errors['category'] : "";
+        $value = isset($user_lot['category']) ? $user_lot['category'] : "";?>
+		<div class="form__item <?=$classname;?>">
           <label for="category">Категория <sup>*</sup></label>
           <select id="category" name="category">
-            <option>Выберите категорию</option>
+            <option value="" selected'>Выберите категорию</option>
             <?php foreach ($categories as $category) : ?>
 			<option><?= esc($category['name']); ?></option>
             <?php endforeach; ?>
@@ -27,7 +34,9 @@
           <span class="form__error">Выберите категорию</span>
         </div>
       </div>
-      <div class="form__item form__item--wide">
+          <?php $classname = isset($errors['message']) ? "form__item--invalid" : "";
+    $error_message = isset($errors['message']) ? $errors['message'] : ""; ?>
+	  <div class="form__item form__item--wide <?=$classname;?>">
         <label for="message">Описание <sup>*</sup></label>
         <textarea id="message" name="message" placeholder="Напишите описание лота"></textarea>
         <span class="form__error">Напишите описание лота</span>
